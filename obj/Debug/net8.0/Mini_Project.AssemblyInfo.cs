1376e9176fabf7a7684ecbb645fef073187cd7cd
sennemander13 <tsennemander@gmail.com>
2024-02-26 14:25:55 +0100

    fix

--- obj/Debug/net8.0/Mini_Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mini_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mini_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b799a9f1e79ba034326604c3b35c693019574c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8402adf9c6a5919f56cfc883497605a2f0ee5ae2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mini_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mini_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
